2b9f5f17677e3f2e406a696cfa15df53f925162c
cjhcj <219939754@qq.com>
2019-11-02 10:44:28 +0800

    state和生命周期

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import { render } from 'react-dom'
 import './App.css'
 
 
-// 函数式组件
-function YourComponent(props) {
-    const formatName = () => {
-        return props.firstName + props.lastName;
+// 类组件
+class Clock extends Component {
+    constructor() {
+        super();
+        this.state = ({
+            date: new Date().getTime()
+        })
     }
-    return (
-        <>
-            {formatName()}
-        </>
-    )
+    tick = () => {
+        this.timer = setInterval(() => {
+            // 这里在setstate这里传入一个回调函数是因为state的更新是异步的，如果你需要用到之前的state里的一些参数，这里最好就是用prestate来获取state里的参数
+            this.setState((preState, props) => {
+                console.log(state, props);
+                return {
+                    date: new Date().getTime()
+                }
+            })
+        }, 1000);
+    }
+    componentWillMount() {
+        this.tick();
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+    render() {
+        return (
+            <div>
+                {this.state.date}
+            </div>
+        )
+    }
 }
 export class App extends Component {
     constructor() {
@@ -21,7 +43,7 @@ export class App extends Component {
     render() {
         return (
             <div>
-                <YourComponent firstName='cjhcj' lastName='zhang' />
+                <Clock />
             </div>
         )
     }
