997db3ecac628f8fead51214f82109406c186765
cjhcj <219939754@qq.com>
2019-11-02 15:17:29 +0800

    列表&key

--- src/App.js
+++ src/App.js
@@ -1,73 +1,45 @@
 import React, { Component } from 'react';
 import './App.css'
 
-function UserGreeting() {
-    return (
-        <h1>
-            hello guys;
-        </h1>
-    )
-}
-
-function GuestGreeging() {
-    return (
-        <h1>
-            please sign up;
-        </h1>
-    )
-}
-
-function Greeting(props) {
-    if (props.isLogin) {
-        return (<UserGreeting></UserGreeting>);
-    } else {
-        return (<GuestGreeging></GuestGreeging>)
-    }
-}
-
-class LoginControl extends Component {
+class Blog extends Component {
     constructor() {
         super();
-        this.state = {
-            isLogin: false
-        }
     }
-    handleLogin() {
-        this.setState({
-            isLogin: true
-        })
-    }
-    handleLogout() {
-        this.setState({
-            isLogin: false
-        })
-    }
-
     render() {
-        let button;
-        const isLogin = this.state.isLogin;
-        if(isLogin) {
-            button = <button onClick={this.handleLogout.bind(this)}>登出</button>;
-        } else {
-            button = <button onClick={this.handleLogin.bind(this)}>登陆</button>;
-        }
+        console.log(this.props.blogs);
+        let blogs = this.props.blogs;
+        const article = (
+            <ul>
+                {
+                    blogs.map((val, index) => {
+                        return (
+                            <li key={val.id}>
+                                {val.content}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
         return (
-            <>
-                <Greeting isLogin={this.state.isLogin} />
-                {button}
-            </>
+            <div>
+                {article}
+            </div>
         )
     }
 }
-
 export class App extends Component {
     constructor() {
         super();
     }
     render() {
+        const data = [
+            { id: 1, title: 'test', content: 'hello test' },
+            { id: 2, title: 'world', content: 'hello world' }
+        ]
         return (
             <div>
-                <LoginControl  />
+                <Blog blogs={data} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom'
 
 import { App } from './App'
 
-setInterval(() => {
-    ReactDom.render(
-        <>
-        <App />
-        </>,
-        document.getElementById('root')
-    
-    )
-}, 100);
+ReactDom.render(
+    <>
+    <App />
+    </>,
+    document.getElementById('root')
+
+)
