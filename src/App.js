b4fd82b8446dfa4cfd0e1aaa227473a79803fd74
cjhcj <219939754@qq.com>
2019-11-02 09:45:50 +0800

    jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
 import './App.css'
+
+const userName = {
+    firstName: 'cjhcj',
+    lastName: 'zhang'
+}
+const formatName = () => {
+    return userName.firstName + userName.lastName;
+}
+// const element = <div>{formatName()}</div>
+
+// Babel 会把 JSX 转译成一个名为 React.createElement() 函数调用
+const element = React.createElement(
+    'div',
+    {className: 'div'},
+    formatName()
+)
+
 export class App extends Component {
     constructor() {
         super();
@@ -8,7 +25,7 @@ export class App extends Component {
     render() {
         return (
             <div>
-                jsx简介
+                {element}
             </div>
         )
     }
