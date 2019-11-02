70217e35242283a04e7206c3520b10d1d7b16ed5
cjhcj <219939754@qq.com>
2019-11-02 16:11:35 +0800

    表单

--- src/App.js
+++ src/App.js
@@ -1,35 +1,81 @@
 import React, { Component } from 'react';
 import './App.css'
 
-class From extends Component {
+// class From extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             name: ''
+//         }
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.handleChange = this.handleChange.bind(this);
+//     }
+//     handleSubmit(event) {
+
+//         console.log('submit !!!!');
+//         event.preventDefault();
+//     }
+//     handleChange(event) {
+//         this.setState({
+//             name: event.target.value
+//         })
+//     }
+//     render() {
+//         return (
+//             <form>
+//                 <label htmlFor="" onSubmit={this.handleSubmit}>
+//                     姓名：
+//                     <input type="text" onChange={this.handleChange} value={this.state.name} placeholder='请输入姓名'/>
+//                 </label>
+//                 <input type="submit" value='提交'/>
+//             </form>
+//         )
+//     }
+// }
+
+class Reservation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
-    handleSubmit(event) {
-        
-        console.log('submit !!!!');
-        event.preventDefault();
-    }
-    handleChange(event) {
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
         this.setState({
-            name: event.target.value
-        })
+            [name]: value
+        });
     }
+
     render() {
         return (
             <form>
-                <label htmlFor="" onSubmit={this.handleSubmit}>
-                    姓名：
-                    <input type="text" onChange={this.handleChange} value={this.state.name} placeholder='请输入姓名'/>
+                <label>
+                    参与:
+            <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
                 </label>
-                <input type="submit" value='提交'/>
+                <br />
+                <label>
+                    来宾人数:
+            <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
             </form>
-        )
+        );
     }
 }
 
@@ -38,10 +84,10 @@ export class App extends Component {
         super();
     }
     render() {
-        
+
         return (
             <div>
-                <From />
+                <Reservation />
             </div>
         )
     }
